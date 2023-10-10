8413d636b8f0f37d03bc5cc033516b07fc794a32
= <muhbintang650@gmail.com>
2023-10-10 12:36:25 +0800

    static field

---
+++ bin/static_field.dart
@@ -0,0 +1,9 @@
+class Application {
+  static final String name = 'Belajar Dart OOP';
+  static final String author = 'Eko Kurniawan';
+}
+
+void main() {
+  print(Application.name); // langsung bisa di ambil
+  print(Application.author);
+}
