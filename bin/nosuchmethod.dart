1f182800cd3321c98e2c8b04120c0d18b3f3711c
= <muhbintang650@gmail.com>
2023-10-10 12:27:26 +0800

    no such method

---
+++ bin/nosuchmethod.dart
@@ -0,0 +1,10 @@
+import 'data/repository.dart';
+
+void main() {
+  var repository = Repository("Product");
+
+  repository.id('1');
+  repository.name('Laptop');
+  repository.quantity(100);
+  repository.location('Makassar');
+}
